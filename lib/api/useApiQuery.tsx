ffb8df27e51b8439d85a6cec007525fc494c3355
Luke <lcq530485521@gmail.com>
2024-02-15 10:23:59 +0800

    complete the functions of txs page (#2)

--- lib/api/useApiQuery.tsx
+++ lib/api/useApiQuery.tsx
@@ -2,8 +2,10 @@ import type { UseQueryOptions } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
 
 import type { Block } from 'types/api/block';
+import type { Transaction } from 'types/api/transaction';
 
 import dayjs from 'lib/date/dayjs';
+import hexToDecimal from 'lib/hexToDecimal';
 import { publicClient } from 'lib/web3/client';
 import { unknownAddress } from 'ui/shared/address/utils';
 
@@ -36,6 +38,7 @@ export default function useApiQuery<R extends ResourceName, E = unknown>(
       // all errors and error typing is handgitled by react-query
       // so error response will never go to the data
       // that's why we are safe here to do type conversion "as Promise<ResourcePayload<R>>"
+      // console.log('queryOptions = ', queryOptions, ', path = ', pathParams, ', query = ', queryParams, ', fetch = ', fetchParams, ', resource = ', resource);
       if (resource === 'blocks') {
         let curBlockNumber = 0;
         let itemsCount = 50;
@@ -93,6 +96,115 @@ export default function useApiQuery<R extends ResourceName, E = unknown>(
 
         const items = await Promise.all(promises);
         data.items = items as Array<Block>;
+
+        return Promise.resolve(data as ResourcePayload<R>);
+      } else if (resource === 'txs_validated') {
+        const MaxItem = 400;
+        let curBlockNumber = 0;
+        let itemsCount = 100;
+        const latestBlock = await publicClient.getBlock();
+        if (queryParams && Boolean(queryParams.block_number)) {
+          curBlockNumber = queryParams.block_number as number;
+          itemsCount = queryParams.items_count as number;
+        } else {
+          curBlockNumber = Number(latestBlock.number);
+        }
+
+        const data = {
+          items: [] as Array<Transaction>,
+          next_page_params: {
+            block_number: 0,
+            items_count: itemsCount,
+          },
+        };
+
+        const items = [];
+        while (curBlockNumber >= 1 && itemsCount > 0 && items.length <= MaxItem) {
+          itemsCount -= 1;
+          curBlockNumber -= 1;
+          const blockParams = { blockNumber: BigInt(curBlockNumber), includeTransactions: true };
+          const block = await publicClient.getBlock(blockParams);
+          const txs = block.transactions
+            .map((tx) => {
+              if (typeof tx === 'string') {
+                return;
+              }
+
+              return {
+                from: { ...unknownAddress, hash: tx.from as string },
+                to: tx.to ? { ...unknownAddress, hash: tx.to as string } : null,
+                hash: tx.hash as string,
+                timestamp: block?.timestamp ? dayjs.unix(Number(block.timestamp)).format() : null,
+                confirmation_duration: null,
+                status: undefined,
+                block: Number(block.number),
+                value: tx.value.toString(),
+                gas_price: tx.gasPrice?.toString() ?? null,
+                base_fee_per_gas: block?.baseFeePerGas?.toString() ?? null,
+                max_fee_per_gas: tx.maxFeePerGas?.toString() ?? null,
+                max_priority_fee_per_gas: tx.maxPriorityFeePerGas?.toString() ?? null,
+                nonce: tx.nonce,
+                position: tx.transactionIndex,
+                type: tx.typeHex ? hexToDecimal(tx.typeHex) : null,
+                raw_input: tx.input,
+                gas_used: null,
+                gas_limit: tx.gas.toString(),
+                confirmations: Number(latestBlock.number - block.number + BigInt(1)),
+                fee: {
+                  value: null,
+                  type: 'actual',
+                },
+                created_contract: null,
+                result: '',
+                priority_fee: null,
+                tx_burnt_fee: null,
+                revert_reason: null,
+                decoded_input: null,
+                has_error_in_internal_txs: null,
+                token_transfers: null,
+                token_transfers_overflow: false,
+                exchange_rate: null,
+                method: null,
+                tx_types: [],
+                tx_tag: null,
+                actions: [],
+              } as Transaction;
+            })
+            .filter(Boolean);
+
+          items.push(...txs);
+        }
+
+        const promises = [];
+        for (const item of items) {
+          const promise = publicClient.getTransactionReceipt({ hash: item.hash as `0x${ string }` }).then((receipt) => {
+            return receipt;
+          });
+          promises.push(promise);
+        }
+        const receipts = await Promise.all(promises);
+        if (items.length === receipts.length) {
+          for (let i = 0; i < items.length; i++) {
+            const txReceipt = receipts[i];
+            const tx = items[i];
+            const status = (() => {
+              if (!txReceipt) {
+                return null;
+              }
+              return txReceipt.status === 'success' ? 'ok' : 'error';
+            })();
+
+            const gasPrice = txReceipt?.effectiveGasPrice ?? tx.gas_price;
+            tx.status = status;
+            tx.gas_price = gasPrice?.toString() ?? null;
+            tx.gas_used = txReceipt?.gasUsed?.toString() ?? null;
+            tx.fee.value = txReceipt && gasPrice ? (txReceipt.gasUsed * BigInt(gasPrice)).toString() : null;
+            tx.created_contract = txReceipt?.contractAddress ? { ...unknownAddress, hash: txReceipt.contractAddress, is_contract: true } : null;
+          }
+        }
+
+        data.items = items as Array<Transaction>;
+        data.next_page_params.block_number = curBlockNumber;
 
         return Promise.resolve(data as ResourcePayload<R>);
       }
